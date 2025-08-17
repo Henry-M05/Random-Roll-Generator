e7bf47746bf4097ec04c79711dfccc7f439e1ab0
HMSLU <hmorgan8@slu.edu>
2025-08-17 15:25:07 -0500

    V. 0.2

--- rollers_test.py
+++ rollers_test.py
@@ -1,7 +1,11 @@
+import numpy
+
 import biased_rolls
 
 
 def main():
+    numpy.set_printoptions(threshold=80000)
+
     manager = biased_rolls.RollerManager()
 
     roller1 = biased_rolls.Roller(1/10, default_rolls = 100)
@@ -16,7 +20,29 @@ def main():
 
     rolls = manager.roll_all()
 
-    print(rolls)
+    # manager.display_rollers()
+
+    # roller2_success = manager.individual_roll_to_success(20000,1)
+    # print(roller2_success)
+
+    # print(manager.all_roll_to_success(20000))
+    # print(rolls)
+
+    manager2 = biased_rolls.RollerManager()
+    roller5 = biased_rolls.Roller(1/8000)
+
+    manager2.add_roller(roller5)
+
+    roll_list = numpy.array((float,int))
+    roll_list.resize(5000)
+
+    manager2.display_rollers()
+
+    for i in range(0,5000):
+        roll_list[i] = (manager2.individual_roll_to_success(20000,0))
+
+    print(roll_list)
+
 
 
 
